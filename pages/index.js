a1cd5cd595b8257b0b94e63425d29e3e1499b05e
andrewc2020 <andrewc2020@gmail.com>
2021-10-29 17:04:04 +0100

    added contact test

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <nav>
+      <Link href="/contact">
+        <a>Contact</a>
+      </Link>
       <Link href="/about">
         <a>About</a>
       </Link>
