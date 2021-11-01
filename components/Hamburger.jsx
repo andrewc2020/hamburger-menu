bb74b430526447fcc0c563e059606a1bd00d73ee
andrewc2020 <andrewc2020@gmail.com>
2021-11-01 10:47:16 +0000

    added click event to menu

--- components/Hamburger.jsx
+++ components/Hamburger.jsx
@@ -1,16 +1,36 @@
 
-
+import Link from "next/link";
 export const Hamburger = () =>{
 
+    const handleClick = (e) => {
+        e.preventDefault()
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+
+        
+
+
+    };
+
+
     return (
-        <body className="bg-gray-50"><header className="flex flex-wrap flex-row justify-between items-center md:space-x-4 bg-white py-6 px-6 relative"><a href='#' className='block'><span className='sr-only'>themes.dev</span>
-        </a><button   className="inline-block md:hidden w-8 h-8 bg-gray-200 text-gray-600 p-1"  ><svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg></button><nav id="myLinks" className="absolute md:relative top-16 left-0 md:top-0 z-20 flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg-white shadow-md rounded-lg md:rounded-none md:shadow-none md:bg-transparent p-6 pt-0 md:p-0"  ><a href='#'>Home</a>
+        <body  className="bg-gray-50"><header className="flex flex-wrap flex-row justify-between items-center md:space-x-4 bg-white py-6 px-6 relative">
+            <a href='#' className='block'><span className='sr-only'>themes.dev</span>
+            </a>
+
+            <button onClick={(e) => handleClick(e)} className="inline-block md:hidden w-8 h-8 bg-gray-200 text-gray-600 p-1"  ><svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg></button>
+            <nav onClick={(e) => handleClick(e)} id="myLinks" className="absolute md:relative top-16 left-0 md:top-0 z-20 flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg-white shadow-md rounded-lg md:rounded-none md:shadow-none md:bg-transparent p-6 pt-0 md:p-0"  ><a href='#'>Home</a>
                 <a className="block py-1 text-gray-600 hover:underline" href='#'>About us</a>
                 <a className="block py-1 text-gray-600 hover:underline" href='#'>Services</a>
                 <a className="block py-1 text-gray-600 hover:underline" href='#'>Blog</a>
                 <a className="block py-1 text-gray-600 hover:underline" href='#'>Contact</a></nav>
-        </header><article className="text-lg px-6 py-6 text-gray-600 mx-auto max-w-2xl"><h2 className="text-xl font-semibold">Page title</h2><p className="mt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet lorem in libero faucibus euismod nec in metus. Quisque at facilisis magna. Donec rhoncus bibendum tincidunt. Vestibulum non orci eget dolor eleifend mollis eu sit amet nunc. Cras malesuada sem nec malesuada cursus.</p><p className="mt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet lorem in libero faucibus euismod nec in metus. Quisque at facilisis magna. Donec rhoncus bibendum tincidunt. Vestibulum non orci eget dolor eleifend mollis eu sit amet nunc. Cras malesuada sem nec malesuada cursus.</p></article>
-       
-            </body>
+        </header>
+            <article className="text-lg px-6 py-6 text-gray-600 mx-auto max-w-2xl"><h2 className="text-xl font-semibold">Page title</h2><p className="mt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet lorem in libero faucibus euismod nec in metus. Quisque at facilisis magna. Donec rhoncus bibendum tincidunt. Vestibulum non orci eget dolor eleifend mollis eu sit amet nunc. Cras malesuada sem nec malesuada cursus.</p><p className="mt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet lorem in libero faucibus euismod nec in metus. Quisque at facilisis magna. Donec rhoncus bibendum tincidunt. Vestibulum non orci eget dolor eleifend mollis eu sit amet nunc. Cras malesuada sem nec malesuada cursus.</p></article>
+
+        </body>
     )
 }
